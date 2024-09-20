bdc48d17510200a78cb944f44c8517d874344cd9
Julian <62448764+OnkelJuls@users.noreply.github.com>
2024-09-20 06:43:45 +0200

    add report and sort characters

--- main.py
+++ main.py
@@ -4,8 +4,28 @@ def main():
     text = get_book_text(book_path)
     word_count = get_word_count(text)
     character_count = get_character_count(text)
-    print(f"{word_count} words found in the document")
-    print(character_count)
+    sorted_character_list = sort_characters(character_count)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the book")
+    print()
+
+    for item in sorted_character_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+def sort_on(d):
+    return d['num']
+
+def sort_characters(character_count):
+    list_of_dict = []
+    for char in character_count:
+        list_of_dict.append({"char": char, "num": character_count[char]})
+    list_of_dict.sort(reverse=True, key=sort_on)
+    return list_of_dict
 
 def get_character_count(text):
     lowered_text = text.lower()
