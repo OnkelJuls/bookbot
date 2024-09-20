e9c06ce774a09d2a14ac7e173657f23e29aa6b2f
Julian <62448764+OnkelJuls@users.noreply.github.com>
2024-09-20 06:10:05 +0200

    add character count

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+# from collections import Counter
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = get_word_count(text)
+    character_count = get_character_count(text)
     print(f"{word_count} words found in the document")
+    print(character_count)
 
+def get_character_count(text):
+    lowered_text = text.lower()
+    # return Counter(lowered_text)
+    dict = {}
+    for x in lowered_text:
+        dict[x] = dict.get(x, 0) + 1
+    return dict
 
 def get_word_count(text):
     words = text.split()
